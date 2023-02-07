b656d5b3152f888a66da5d4362d8b8445ac7d309
Md Sholayman <mdsholayman998@gmail.com>
2023-02-07 19:19:21 +0600

    data loaded

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use(cors());
+app.use(express());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  `mongodb+srv://${process.env.PS_USER}:${process.env.PS_PASS}@cluster0.f2xkrlu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run () {
+try{
+    await client.connect()
+    const movieCollection = client.db('popCritic').collection('movies')
+
+    app.get("/movies" , async(req,res) =>{
+        const movies = await movieCollection.find({}).toArray()
+        res.send(movies)
+    })
+}
+finally{
+    
+
+}
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
